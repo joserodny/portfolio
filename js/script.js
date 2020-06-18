b986fe3224642b68f3499505e6a4a1668005e210
jose rodny sibugan <41980624+joseeodny@users.noreply.github.com>
2020-06-19 00:02:13 +0800

    portfolio 2020

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let animations = bodymovin.loadAnimation({
+    container: document.getElementById('anim'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../Animation/drawkit-grape-animation-2-LOOP.json' // the path to the animation json
+});
